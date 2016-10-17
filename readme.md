6e9ca549a308e97f4aeed097e874d2dca9f752d0
devjanaprime <dev@primeacademy.io>
2016-10-17 14:05:05 -0500

    added basic shuffle functionality

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 Cryptogramicon
 ==============
 ![ash](http://cdn.mos.cms.futurecdn.net/a521800002d36e37ee329a424668b324.jpg)]
+
+A Cryptogram maker and taker app in JS.
---
+++ scripts/cryptogramicon.js
@@ -0,0 +1,10 @@
+// cryptogramicon.js
+// by @devjana
+// https://github.com/devjanaprime/cryptogramicon
+var verbose = true;
+if( verbose ) console.log( 'cryptogramicon.js sourced' );
+
+var alphabet = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+
+var shuffledAlphabet = shuffle( alphabet );
+console.log( shuffledAlphabet );
